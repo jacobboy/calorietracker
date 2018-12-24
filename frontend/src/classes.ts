cf1fb066a4c1ad84588f23263aab902e995bec64
Jake Boy <jacob@picwell.com>
2018-12-24 16:21:03 -0500

    Return object from NDB api

--- frontend/src/classes.ts
+++ frontend/src/classes.ts
@@ -143,7 +143,6 @@ class NDBIngredient extends AbstractNutritional implements NDBed {
   readonly uid: string;
 
   static fromReport(report: Report): NDBIngredient {
-    console.log(report);
     const ndbno = report.food.ndbno;
     const name = report.food.name;
     const fat = report.fat;
--- frontend/src/ndbapi/index.ts
+++ frontend/src/ndbapi/index.ts
@@ -15,27 +15,32 @@ function searchUrl() {
   return 'https://api.nal.usda.gov/ndb/search/?api_key=' + GOV_API_KEY;
 }
 
-function fetchSearch(searchString: string, dataSource: DataSource): Promise<SearchList> {
-  const body: { q: string, ds?: string, max?: string, offset?: string } = {
-    'q': searchString,
+function fetchSearch(
+  searchString: string,
+  dataSource: DataSource
+): Promise<SearchList> {
+  const body: { q: string; ds?: string; max?: string; offset?: string } = {
+    q: searchString
   };
   if (dataSource !== DataSource.Any) {
-    body.ds = dataSource === DataSource.SR ? 'Standard Reference' : 'Branded Food Products';
+    body.ds =
+      dataSource === DataSource.SR
+        ? 'Standard Reference'
+        : 'Branded Food Products';
   }
   const bodyStr = JSON.stringify(body);
   // console.log('Searching for\n' + bodyStr);
-  return fetch(
-    searchUrl(), {
-      method: 'POST',
-      body: bodyStr,
-      headers: new Headers({ 'Content-type': 'application/json' }),
-      credentials: 'omit'
-    })
-    .then((response) => {
+  return fetch(searchUrl(), {
+    method: 'POST',
+    body: bodyStr,
+    headers: new Headers({ 'Content-type': 'application/json' }),
+    credentials: 'omit'
+  })
+    .then(response => {
       // console.log('Got response\n' + JSON.stringify(response));
       return response.json();
     })
-    .then((response) => {
+    .then(response => {
       // console.log('Got response\n' + JSON.stringify(response));
       if (response.errors) {
         return { item: [] };
@@ -48,31 +53,28 @@ function fetchSearch(searchString: string, dataSource: DataSource): Promise<Sear
 
 export function queryReport(ndbno: string): Promise<Report> {
   const url = reportUrl();
-  const body = JSON.stringify({ 'ndbno': ndbno, 'type': 'b' });
+  const body = JSON.stringify({ ndbno: ndbno, type: 'b' });
   // console.log('Getting ' + ndbno + ' from ' + url);
   // console.log(body);
-  return fetch(
-    url, {
-      method: 'POST',
-      body: body,
-      headers: new Headers({ 'Content-type': 'application/json' }),
-      credentials: 'omit',
-      mode: 'cors'
-    }).then((response) => response.json())
-    .then((response) => response.report);
+  return fetch(url, {
+    method: 'POST',
+    body: body,
+    headers: new Headers({ 'Content-type': 'application/json' }),
+    credentials: 'omit',
+    mode: 'cors'
+  })
+    .then(response => response.json())
+    .then(response => Report.new(response.report));
 }
 
 export function searchFood(
   searchString: string,
-  dataSource: DataSource): Promise<IngredientSearchItem[]> {
-  return fetchSearch(
-    searchString,
-    dataSource
-  ).then(
-    (searchList) => searchList.item.map(
-      (sli) => {
-        // console.log('Got search');
-        return IngredientSearchItem.fromSearchListItem(sli);
-      }
-    ));
+  dataSource: DataSource
+): Promise<IngredientSearchItem[]> {
+  return fetchSearch(searchString, dataSource).then(searchList =>
+    searchList.item.map(sli => {
+      // console.log('Got search');
+      return IngredientSearchItem.fromSearchListItem(sli);
+    })
+  );
 }
