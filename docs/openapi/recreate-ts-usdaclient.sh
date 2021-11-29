c6c43e14c1f0be05fc6ddcbc39a7341ae073498b
Jake Boy <boy.jacob@gmail.comx>
2021-11-29 10:38:18 -0600

    get new usda api spec

--- docs/openapi/recreate-ts-usdaclient.sh
+++ docs/openapi/recreate-ts-usdaclient.sh
@@ -1,1 +1,7 @@
-openapi-generator generate -i usda-api.yaml -g typescript-fetch -o usdaclient && rm -rf ../../frontend/src/usdaclient && mv usdaclient ../../frontend/src/usdaclient
+# https://stackoverflow.com/a/246128
+# get current directory of script, regardless of how i'm being executed
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/"
+
+openapi-generator generate -i ${DIR}/fdcnal-food-data_central_api-1.0.0-swagger.json -g typescript-axios -o ${DIR}/../../fev2/macromacro/src/
+
+# openapi-generator generate -i usda-api.yaml -g typescript-fetch -o usdaclient && rm -rf ../../frontend/src/usdaclient && mv usdaclient ../../frontend/src/usdaclient
