5741447b7047e4492ef98eace5ff845ca53e619b
Jake Boy <boy.jacob@gmail.comx>
2021-11-29 13:33:33 -0600

    CORS Issue?

--- docs/openapi/recreate-ts-usdaclient.sh
+++ docs/openapi/recreate-ts-usdaclient.sh
@@ -2,6 +2,6 @@
 # get current directory of script, regardless of how i'm being executed
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/"
 
-openapi-generator generate -i ${DIR}/fdcnal-food-data_central_api-1.0.0-swagger.json -g typescript-axios -o ${DIR}/../../fev2/macromacro/src/
+openapi-generator generate -i ${DIR}/fdcnal-food-data_central_api-1.0.0-swagger.json -g typescript-axios -o ${DIR}/../../fev2/macromacro/src/usda --api-package usdaclient --model-package usdamodel
 
 # openapi-generator generate -i usda-api.yaml -g typescript-fetch -o usdaclient && rm -rf ../../frontend/src/usdaclient && mv usdaclient ../../frontend/src/usdaclient
--- fev2/macromacro/src/App.tsx
+++ fev2/macromacro/src/App.tsx
@@ -1,25 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import { initializeApp } from 'firebase/app';
+import Input from '@mui/material/Input';
+// import { initializeApp } from 'firebase/app';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Configuration, FDCApi, SearchResultFood } from "./usda";
 
+const ariaLabel = { 'aria-label': 'description' };
 
 function App() {
+  const [searchText, setSearchText] = useState('');
+  const [searchResults, setSearchResults] = useState<SearchResultFood[]>([]);
+
+  function createData(searchResult: SearchResultFood) {
+      return {
+        name: searchResult.description,
+        calories: 0,
+        protein: 0,
+        carbs: 0,
+        fat: 0
+      }
+  }
+
+  async function search() {
+    console.log('searching');
+    if (searchText) {
+      console.log('has search teext')
+      const config = new Configuration({
+        apiKey: 'bu776D0hQ8ZBGC3g1eoUB3iNwknI6MJhNo1xzwRh',
+        baseOptions: {
+          withCredentials: false
+        }
+      })
+      const api = new FDCApi(config)
+      api.getFoodsSearch(searchText).then(
+          (response) => {
+            if (response.data.foods) {
+              setSearchResults(response.data.foods)
+              console.log(searchResults)
+            } else {
+              console.log('nothing found')
+            }
+          }
+      ).catch((error) => {
+        console.log('oops')
+      })
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={search}>
+          <Input placeholder="Placeholder" value={searchText} onChange={e => setSearchText(e.target.value)} inputProps={ariaLabel} />
+          <input type="submit" value="Submit" />
+        </form>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Food (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {searchResults.map(createData).map((row) => (
+                  <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">{row.name}</TableCell>
+                    <TableCell align="right">{row.calories}</TableCell>
+                    <TableCell align="right">{row.fat}</TableCell>
+                    <TableCell align="right">{row.carbs}</TableCell>
+                    <TableCell align="right">{row.protein}</TableCell>
+                  </TableRow>
+                  )
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </header>
     </div>
   );
