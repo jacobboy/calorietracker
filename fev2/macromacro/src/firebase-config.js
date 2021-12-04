b68d29ee39be29c5c16742959a90ee90ac1b152f
Jake Boy <boy.jacob@gmail.comx>
2021-12-04 16:21:42 -0600

    start adding firebase?

--- fev2/macromacro/src/firebase-config.js
+++ fev2/macromacro/src/firebase-config.js
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCoA1jJWPwnpoA6H0iHuRu5v9b6Xje1k8I",
-  authDomain: "macromacro.firebaseapp.com",
-  databaseURL: "https://macromacro.firebaseio.com",
-  projectId: "macromacro",
-  storageBucket: "macromacro.appspot.com",
-  messagingSenderId: "706551309842",
-  appId: "1:706551309842:web:44fddd6f26da94ce2adacb",
-  measurementId: "G-BBWYX68GKG"
+  apiKey: "AIzaSyAhA_vPB41PhAvGy4-AfHaQPdJXrMPKG6w",
+  authDomain: "macro-macro.firebaseapp.com",
+  projectId: "macro-macro",
+  storageBucket: "macro-macro.appspot.com",
+  messagingSenderId: "98127882402",
+  appId: "1:98127882402:web:3844a154a18c4c080ecffb",
+  measurementId: "G-HCMQNGPE9P"
 };
 
 // Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
